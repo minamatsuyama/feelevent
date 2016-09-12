7d402add000ef73db30bf8ab608141bb3319d396
iyuuya <vim.ito@1pac.jp>
2016-09-12 11:57:05 +0900

    ユーザー用のdevise-controllerを生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
-  devise_for :users, class_name: "Event::User"
+  devise_for :users, class_name: "Event::User", controllers: {
+    confirmations: 'users/registrations',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+  }
+
   namespace :admin do
     devise_for :users, class_name: "Event::Admin::User"
   end
