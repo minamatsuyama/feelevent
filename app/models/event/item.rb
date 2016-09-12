10af36de30cde3947e5e102730f0447131270d22
iyuuya <vim.ito@1pac.jp>
2016-09-12 14:50:10 +0900

    キーワードのassociationを追加

--- app/models/event/item.rb
+++ app/models/event/item.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class Event::Item < ApplicationRecord
   belongs_to :event_admin_user, class_name: 'Event::Admin::User'
+  has_and_belongs_to_many :keywords, foreign_key: 'event_item_id'
 
   extend Enumerize
 
--- spec/models/event/item_spec.rb
+++ spec/models/event/item_spec.rb
@@ -4,5 +4,6 @@ require 'rails_helper'
 RSpec.describe Event::Item, type: :model do
   describe 'associations' do
     it { is_expected.to belong_to(:event_admin_user).class_name('Event::Admin::User') }
+    it { is_expected.to have_and_belong_to_many(:keywords).with_foreign_key('event_item_id') }
   end
 end
