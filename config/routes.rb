7f76e6ebc9c62e716b0cb4d2c5212ed52070c98e
iyuuya <vim.ito@1pac.jp>
2016-09-12 17:14:09 +0900

    deviseのコントローラー名が異なるのを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   devise_for :users, class_name: 'Event::User', controllers: {
-    confirmations: 'users/registrations',
+    confirmations: 'users/confirmations',
     passwords: 'users/passwords',
     registrations: 'users/registrations',
     sessions: 'users/sessions'
