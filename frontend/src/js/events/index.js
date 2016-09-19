05d7ba490d9efa4a0ccfdf7ab9b8f6a1de6b0ca7
2015mac08 <vane.diem@1pac.jp>
2016-09-19 18:07:09 +0700

    modify events detail page design and add carousel

---
+++ frontend/src/js/events/index.js
@@ -0,0 +1,23 @@
+//Carousel for events detail page
+
+$(document).ready(function () {
+  var $window = $(window)
+    , $card = $('.recommend-list')
+    , toggleSlick;
+
+  toggleSlick = function () {
+    if ($window.width() < 450) {
+      $card.slick({
+        dots: false,
+        infinite: true,
+        speed: 600,
+        arrows: false,
+      });
+    } else {
+      $card.unslick();
+    }
+  }
+
+  $window.resize(toggleSlick);
+  toggleSlick();
+});
