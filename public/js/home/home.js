14bc823ea4dfcd77478b900b4e37de37cad7bdb1
max.phong <max.phong@1pac.vn>
2016-09-21 18:00:21 +0700

    update: - Status hover and active of keyword - Update Recommend tag - Update 9 event cards - Add load more on bottom - Clickable calendar icon

---
+++ public/js/home/home.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function() {
+    mainSlideshowInit();
+    datepickerInit();
+    select2Init();
+    $(".main-slideshow").on("setPosition", function() {
+        arrowsPosition();
+    });
+});
+
+function mainSlideshowInit() {
+    $(".main-slideshow").slick({
+        centerMode: true,
+        slidesToShow: 1,
+        centerPadding: 0,
+        variableWidth: true
+    });
+}
+
+function arrowsPosition() {
+    var windowsWidth = $(window).width();
+    centerWidth = $(".slick-center").width();
+    centerPadding = (windowsWidth - centerWidth) / 2;
+    arrowWidth = $(".slick-arrow").width();
+    $(".slick-prev").css("left", centerPadding - arrowWidth / 2);
+    $(".slick-next").css("right", centerPadding - arrowWidth / 2);
+}
+
+function datepickerInit() {
+    $(".search-area .date-picker input").datepicker($.datepicker.regional["ja"]);
+}
+
+function select2Init() {
+    $(".selectpicker select").select2({
+        minimumResultsForSearch: Infinity
+    });
+}
