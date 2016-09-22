cc6e6c86b8528d9e2a581cc11601a98300fdba0d
Minh Phan <wofi.minh@1pac.jp>
2016-09-22 10:04:00 +0700

    Add creating favorite event API

---
+++ app/controllers/api/favorites_controller.rb
@@ -0,0 +1,15 @@
+class Api::FavoritesController < ApplicationController
+  def create
+    render json: { "message": "Unauthorized" } and return unless user_signed_in?
+
+    event_item = Event::Item.get_valid_item(params[:id])
+    render json: { "message": "Not Found" }, status: 404 and return if event_item.blank?
+
+    favorites = event_item.favorites.where("event_user_id = ?", current_user.id)
+    render json: { "message": "Conflict" }, status: 409 and return if favorites.present?
+
+    favorite = Event::Favorite.new({ event_user_id: current_user.id, event_item_id: params[:id] })
+    favorite.save!
+    render json: { event_item_id: params[:id], count: favorites.count() }, status: 201
+  end
+end
