08ea386cc4c51d535be6248e38fca652b56d7b76
mars-tran <mars.tran@1pac.jp>
2016-09-23 15:55:52 +0900

    Add 管理画面のルータ設定

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,24 @@ Rails.application.routes.draw do
     sign_up: 'signup'
   }
 
+  ## 管理画面用
+  ### ログイン係
+  devise_for :admin_users, class_name: 'Event::Admin::User', path: 'admin', controllers: {
+    sessions: 'admin/users/sessions',
+    passwords: 'admin/users/passwords'
+  }, path_names:  {
+    sign_in: 'signin',
+    sign_out: 'signout',
+    sign_up: 'new'
+  }
+
+  authenticated(:admin_user) do
+    root to: 'admin/events#index', as: :admin_authenticated_root
+  end
+
   namespace :admin do
-    devise_for :users, class_name: 'Event::Admin::User'
+    resources :users, param: :event_admin_user_id
+    get 'users/confirmations' => 'users/confirmations#show'
+    resources :events, only: [:index]
   end
 end
