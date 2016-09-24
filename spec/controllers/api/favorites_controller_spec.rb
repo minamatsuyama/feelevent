fc23c597a7f0e60d153cb5dfa41dbcf83c04a932
Minh Phan <wofi.minh@1pac.jp>
2016-09-24 23:45:53 +0700

    Fix test case

--- spec/controllers/api/favorites_controller_spec.rb
+++ spec/controllers/api/favorites_controller_spec.rb
@@ -21,7 +21,7 @@ describe Api::FavoritesController do
       expect(parsed_body["data"]).to be_empty
     end
     it "get event_item result" do
-      event_item = FactoryGirl.create(:event_item, published: true)
+      event_item = FactoryGirl.create(:event_item, published: true, publish_started_at: Time.current - 1.days, publish_ended_at: Time.current + 1.days)
       get :index
       parsed_body = JSON.parse(response.body)
       expect(parsed_body["data"]).not_to be_empty
