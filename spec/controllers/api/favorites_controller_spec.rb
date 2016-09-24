f9dfbe37befc3ed474a00d1bcfb36dac672705c4
Minh Phan <wofi.minh@1pac.jp>
2016-09-24 23:37:05 +0700

    Add delete favorite event test

--- spec/controllers/api/favorites_controller_spec.rb
+++ spec/controllers/api/favorites_controller_spec.rb
@@ -1,18 +1,43 @@
 require "rails_helper"
 
 describe Api::FavoritesController do
-  describe "DELETE #delete" do
-    before(:each) do
-      @request.env["devise.mapping"] = Devise.mappings[:user]
-      user = FactoryGirl.create(:event_user)
-      sign_in user
+  describe "DELETE #destroy" do
+    before(:each) do |testcase|
+      unless testcase.metadata[:skip_before]
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        user = FactoryGirl.create(:event_user)
+        sign_in user
+      end
+     end
+    it "is not signed in", skip_before: true do
+      event_item = FactoryGirl.create(:event_item)
+      delete :destroy, params: { id: event_item[:id] }
+      parsed_body = JSON.parse(response.body)
+      expect(response.status).to eq(401)
+      expect(parsed_body["message"]).to eq("Unauthorized")
     end
-    it "should have a current_user" do
-      expect(subject.current_user).to_not eq(nil)
+    it "is invalid event item" do
+      event_item = FactoryGirl.create(:event_item, published: false)
+      delete :destroy, params: {id: event_item[:id]}
+      parsed_body = JSON.parse(response.body)
+      expect(response.status).to eq(404)
+      expect(parsed_body["message"]).to eq("Not Found")
     end
-    it "require user signed in"
-    it "require valid event_item"
-    it "require event_favorite existed"
-    it "delete event_favorite"
+    it "has no favorite event" do
+      event_item = FactoryGirl.create(:event_item, published: true, publish_started_at: Time.current - 1.days, publish_ended_at: Time.current + 1.days)
+      event_favorite = FactoryGirl.create(:event_favorite, event_item_id: event_item[:id], event_user_id: subject.current_user[:id] + 1)
+      delete :destroy, params: {id: event_item[:id]}
+      parsed_body = JSON.parse(response.body)
+      expect(response.status).to eq(404)
+      expect(parsed_body["message"]).to eq("Not Found")
+    end
+    it "delete favorite event" do
+      event_item = FactoryGirl.create(:event_item, published: true, publish_started_at: Time.current - 1.days, publish_ended_at: Time.current + 1.days)
+      event_favorite = FactoryGirl.create(:event_favorite, event_item_id: event_item[:id], event_user_id: subject.current_user[:id])
+      delete :destroy, params: {id: event_item[:id]}
+      parsed_body = JSON.parse(response.body)
+      expect(response.status).to eq(200)
+      expect(parsed_body["event_item_id"]).not_to be_empty
+     end
   end
 end
