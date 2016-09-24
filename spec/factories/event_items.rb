e38a0679bb48c0fe61c3b51ab042752e5dc6cc00
Minh Phan <wofi.minh@1pac.jp>
2016-09-24 21:23:54 +0700

    Fix event_item factory

--- spec/factories/event_items.rb
+++ spec/factories/event_items.rb
@@ -2,10 +2,10 @@
 FactoryGirl.define do
   factory :event_item, class: 'Event::Item' do
     event_admin_user_id  { Faker::Number.between(1, 10000) }
-    event_type_id        { Faker::Number.between(1, 10000) }
-    held_started_at      { Faker::Time.between(Time.current, Time.current + 1.month) }
+    event_type_id        { Faker::Number.between(1, 3) }
+    held_started_at      { Faker::Time.between(Time.current - 20.days, Time.current) }
     held_ended_at        { held_started_at + Faker::Number.between(1, 10).days }
-    entry_started_at     { Faker::Time.between(Time.current, Time.current + 1.month) }
+    entry_started_at     { Faker::Time.between(Time.current - 20.days, Time.current) }
     entry_ended_at       { entry_started_at + Faker::Number.between(1, 10).days }
     entry_fee            { Faker::Number.decimal(2) }
     currency             { Event::Item.currency.values.sample }
@@ -13,8 +13,8 @@ FactoryGirl.define do
     preparation_type     { Event::Item.preparation_type.values.sample }
     index_image          { nil }
     detail_image         { nil }
-    publish_started_at   { Faker::Time.between(Time.current, Time.current + 1.month) }
-    publish_ended_at     { publish_started_at + Faker::Number.between(1, 10).days }
+    publish_started_at   { Faker::Time.between(Time.current - 20.days, Time.current) }
+    publish_ended_at     { publish_started_at + Faker::Number.between(21, 30).days }
     published            { Faker::Boolean.boolean }
     title                { Faker::Book.title }
     summary              { Faker::Lorem.paragraph }
