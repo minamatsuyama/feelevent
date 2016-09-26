77dd388997ce37d048477ab0a3277183ac372018
Thai Thi Yen Nhi <skye.nhi@1pac.vn>
2016-09-26 14:23:34 +0700

    Merge branch 'master', fix html structure, fix design SP, change select box (use select2), add js for select box, checkbox.

---
+++ frontend/src/js/events/index.js
@@ -0,0 +1,48 @@
+//Carousel for events detail page
+
+$(document).ready(function () {
+
+  initSlick();
+  initLoadMore();
+
+});
+
+function initSlick() {
+  var $window = $(window)
+    , $card = $('.recommend-list')
+    , toggleSlick;
+
+  toggleSlick = function () {
+    if ($(window).width() < 480) {
+      if($card.hasClass('slick-initialized')) {
+        $card.slick("unslick");
+      }
+      $card.slick({
+        infinite: true,
+        speed: 600,
+        dots: false,
+        arrows: false,
+        variableWidth: true
+      });
+    } else {
+      if($card.hasClass('slick-initialized')) {
+        $card.slick("unslick");
+      }
+    }
+  };
+
+  $window.resize(toggleSlick);
+  toggleSlick();
+}
+
+function initLoadMore() {
+  var $btn = $('.recommends .btn-more');
+  var $eventcard = $('.recommend-list .eventcard');
+  $btn.on('click',function (e) {
+    $('.recommend-list .eventcard:hidden').slice(0, 3).css("display","inline-block");
+    if ($eventcard.length == $('.recommend-list .eventcard:visible').length) {
+      $btn.hide();
+    }
+    e.preventDefault();
+  });
+}
