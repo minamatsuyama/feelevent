a5ebafce0e9da0b222dc495191121fef5594eeec
mars-tran <mars.tran@1pac.jp>
2016-09-26 11:35:16 +0900

    Add 401エラー追加
    401 Unauthorized (Only Json)

--- app/concerns/app_errors.rb
+++ app/concerns/app_errors.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 module AppErrors
+  class Error401 < StandardError; end
   class Error403 < StandardError; end
   class Error404 < StandardError; end
   class Error500 < StandardError; end
