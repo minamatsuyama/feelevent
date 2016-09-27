184d5a302f477cebbfed43c17d454505de432b2a
harunapple <harunapple@gmail.com>
2016-09-28 07:25:05 +0900

    法人が登録したキーワードを取得するスコープのテストを追加

--- spec/models/keyword_spec.rb
+++ spec/models/keyword_spec.rb
@@ -2,4 +2,9 @@
 require 'rails_helper'
 
 RSpec.describe Keyword, type: :model do
+  describe 'scopes' do
+    describe '#owner_managed' do
+      it { expect(Keyword.owner_managed.to_sql).to eq Keyword.where(company_id: nil).to_sql }
+    end
+  end
 end
