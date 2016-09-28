67a379bb23d71dbf56d1a963bd0c5f5f4f8f5c14
Minh Phan <wofi.minh@1pac.jp>
2016-09-28 10:23:00 +0700

    Beautify code follow rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     devise_for :users, class_name: 'Event::Admin::User'
   end
 
-  namespace :api, defaults: {format: :json} do
-    delete "favorites/:id"                           => "favorites#destroy", constraints: { id: /\d+/ }
+  namespace :api, defaults: { format: :json } do
+    delete 'favorites/:id' => 'favorites#destroy', constraints: { id: /\d+/ }
   end
 end
--- spec/controllers/api/favorites_controller_spec.rb
+++ spec/controllers/api/favorites_controller_spec.rb
@@ -1,43 +1,44 @@
-require "rails_helper"
+# frozen_string_literal: true
+require 'rails_helper'
 
 describe Api::FavoritesController do
-  describe "DELETE #destroy" do
+  describe 'DELETE #destroy' do
     before(:each) do |testcase|
       unless testcase.metadata[:skip_before]
-        @request.env["devise.mapping"] = Devise.mappings[:user]
+        @request.env['devise.mapping'] = Devise.mappings[:user]
         user = FactoryGirl.create(:event_user)
         sign_in user
       end
-     end
-    it "is not signed in", skip_before: true do
-      event_item = FactoryGirl.create(:event_item)
-      delete :destroy, params: { id: event_item[:id] }
+    end
+    it 'is not signed in', skip_before: true do
+      event_item = FactoryGirl.create(:event_item, published: false)
+      delete :destroy, params: { id: event_item[:id] }, format: :json
       parsed_body = JSON.parse(response.body)
       expect(response.status).to eq(401)
-      expect(parsed_body["message"]).to eq("Unauthorized")
+      expect(parsed_body['message']).to eq('Unauthorized')
     end
-    it "is invalid event item" do
+    it 'is invalid event item' do
       event_item = FactoryGirl.create(:event_item, published: false)
-      delete :destroy, params: {id: event_item[:id]}
+      delete :destroy, params: { id: event_item[:id] }, format: :json
       parsed_body = JSON.parse(response.body)
       expect(response.status).to eq(404)
-      expect(parsed_body["message"]).to eq("Not Found")
+      expect(parsed_body['message']).to eq('Not Found')
     end
-    it "has no favorite event" do
-      event_item = FactoryGirl.create(:event_item, published: true, publish_started_at: Time.current - 1.days, publish_ended_at: Time.current + 1.days)
-      event_favorite = FactoryGirl.create(:event_favorite, event_item_id: event_item[:id], event_user_id: subject.current_user[:id] + 1)
-      delete :destroy, params: {id: event_item[:id]}
+    it 'has no favorite event' do
+      event_item = FactoryGirl.create(:event_item, published: true, publish_started_at: Time.current - 1.day, publish_ended_at: Time.current + 1.day)
+      FactoryGirl.create(:event_favorite, event_item_id: event_item[:id], event_user_id: subject.current_user[:id] + 1)
+      delete :destroy, params: { id: event_item[:id] }, format: :json
       parsed_body = JSON.parse(response.body)
       expect(response.status).to eq(404)
-      expect(parsed_body["message"]).to eq("Not Found")
+      expect(parsed_body['message']).to eq('Not Found')
     end
-    it "delete favorite event" do
-      event_item = FactoryGirl.create(:event_item, published: true, publish_started_at: Time.current - 1.days, publish_ended_at: Time.current + 1.days)
-      event_favorite = FactoryGirl.create(:event_favorite, event_item_id: event_item[:id], event_user_id: subject.current_user[:id])
-      delete :destroy, params: {id: event_item[:id]}
+    it 'delete favorite event' do
+      event_item = FactoryGirl.create(:event_item, published: true, publish_started_at: Time.current - 1.day, publish_ended_at: Time.current + 1.day)
+      FactoryGirl.create(:event_favorite, event_item_id: event_item[:id], event_user_id: subject.current_user[:id])
+      delete :destroy, params: { id: event_item[:id] }
       parsed_body = JSON.parse(response.body)
       expect(response.status).to eq(200)
-      expect(parsed_body["event_item_id"]).not_to be_empty
-     end
+      expect(parsed_body['event_item_id']).not_to be_empty
+    end
   end
 end
