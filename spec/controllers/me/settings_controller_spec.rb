951c25a28e3537e82793e4008d0f6c54ab3f2b84
iyuuya <vim.ito@1pac.jp>
2016-09-28 13:13:16 +0900

    rubocop auto-correct

--- spec/controllers/me/settings_controller_spec.rb
+++ spec/controllers/me/settings_controller_spec.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe Me::SettingsController, type: :controller do
-  describe "GET #edit" do
-    it "returns http success" do
+  describe 'GET #edit' do
+    it 'returns http success' do
       get :edit
       expect(response).to have_http_status(:success)
     end
