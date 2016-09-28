5690b272b7dd55489e82106d82f08bc7cb03363b
mars-tran <mars.tran@1pac.jp>
2016-09-28 13:47:46 +0900

    Modify letを使い，発行したユーザを後でも使えるように

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 module LoginMacros
   def login_as_admin(user_type = :normal_user)
+    let(:admin_user) { create :event_admin_user, user_type }
     before(:each) do
       @request.env['devise.mapping'] = Devise.mappings[:admin_user]
-      sign_in FactoryGirl.create(:event_admin_user, user_type)
+      sign_in admin_user
     end
   end
 
   def login_as_user
+    let(:user) { create :event_user }
     before(:each) do
       @request.env['devise.mapping'] = Devise.mappings[:user]
-      sign_in FactoryGirl.create(:event_user)
+      sign_in user
     end
   end
 end
