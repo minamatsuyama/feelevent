c46a46583644a2d4b95dd264df1539d1aa51ccde
Minh Phan <wofi.minh@1pac.jp>
2016-09-28 10:22:32 +0700

    Improve destroy favorite event API code

--- app/controllers/api/favorites_controller.rb
+++ app/controllers/api/favorites_controller.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
 class Api::FavoritesController < ApplicationController
-  def destroy
-    render json: { "message": "Unauthorized" }, status: 401 and return unless user_signed_in?
+  before_action :authenticate_user!, only: [:destroy]
 
+  def destroy
     event_item = Event::Item.where(id: params[:id]).published.in_time
-    render json: { "message": "Not Found" }, status: 404 and return if event_item.blank?
+    raise AppErrors::Error404 if event_item.blank?
 
     favorite = event_item[0].event_favorites.where(event_user_id: current_user.id)
-    render json: { "message": "Not Found" }, status: 404 and return if favorite.blank?
+    raise AppErrors::Error404 if favorite.blank?
 
     favorite[0].destroy
-    render json: { event_item_id: params[:id], count: event_item[0].event_favorites.count() }, status: 200
+    render json: { event_item_id: params[:id], count: event_item[0].event_favorites.count }, status: 200
   end
 end
